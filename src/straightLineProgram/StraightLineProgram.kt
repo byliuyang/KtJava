8a68f3815e219b58ddc6b9735f5a948637d8f2db
Yang Liu <byliuyang11@gmail.com>
2018-03-04 16:05:15 -0500

    Add maxArgs

--- src/straightLineProgram/StraightLineProgram.kt
+++ src/straightLineProgram/StraightLineProgram.kt
@@ -6,6 +6,54 @@ import straightLineProgram.expressions.*
 import straightLineProgram.statements.AssignStatement
 import straightLineProgram.statements.CompoundStatement
 import straightLineProgram.statements.PrintStatement
+import straightLineProgram.statements.Statement
+import java.lang.IllegalArgumentException
+
+fun maxArgs(expression: Expression): Int {
+    when (expression) {
+        is OperatorExpression -> {
+            return Math.max(maxArgs(expression.expressionLeft), maxArgs(expression.expressionRight))
+        }
+        is SequenceExpression -> {
+            return Math.max(maxArgs(expression.statement), maxArgs(expression.expression))
+        }
+        else -> return@maxArgs 0
+    }
+}
+
+/**
+ * Tells the maximum num- ber of arguments of any print statement within any subexpression of a given statement.
+ *
+ * For example,
+ * prog => a := 5+3; b := (print(a, a-1), 10*a); print(b)
+ * maxargs(prog) is 2
+ */
+fun maxArgs(statement: Statement): Int {
+    when (statement) {
+        is AssignStatement -> {
+            return maxArgs(statement.expression)
+        }
+        is CompoundStatement -> {
+            return Math.max(maxArgs(statement.statementLeft), maxArgs(statement.statementRight))
+        }
+        is PrintStatement -> {
+            var count = 0
+            var maxCount = 0
+            var expressionList = statement.expressionList
+            while (expressionList !is LastExpressionList) {
+                count++
+                if (expressionList is PairExpressionList) {
+                    maxCount = Math.max(maxCount, maxArgs(expressionList.head))
+                    expressionList = expressionList.tail
+                } else throw IllegalArgumentException("Unknown expressionList")
+            }
+
+            maxCount = Math.max(maxCount, maxArgs(expressionList.head))
+            return Math.max(maxCount, count + 1)
+        }
+        else -> throw IllegalArgumentException("Unknown Statement")
+    }
+}
 
 fun main(args: Array<String>) {
     // a := 5+3; b := (print(a, a-1), 10*a); print(b)
@@ -47,4 +95,6 @@ fun main(args: Array<String>) {
                             )
                     )
             ))
+
+    print(maxArgs(program))
 }
